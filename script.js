13b239864d446714ba08eb14581c200d16509e19
gentlegiantsmk15 <72901407+gentlegiantsmk15@users.noreply.github.com>
2020-10-24 12:34:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -77,23 +77,28 @@ function Process(response){
     sendMessage(response)
   } else if (response.message.photo){
     sendPhoto(response);
+  }else if (response.message.document){
+    sendDocument(response);
   }
 }
 
 async function sendMessage(response) {
-  try {
-    var params = {chat_id: response.message.chat.id, text: response.message.text, reply_to_message_id:response.message.message_id};
-    var url = telegramUrl + "/sendMessage";
-    var res = await FecthPost(url,params);
-    MessageSent(res);
-  }catch(err) {
-  sendMessage();
-  }
-}
-
+  var params = {chat_id: response.message.chat.id, text: response.message.text, reply_to_message_id:response.message.message_id};
+  var url = telegramUrl + "/sendMessage";
+  var res = await FecthPost(url,params);
+  MessageSent(res);
+}  
+  
 async function sendPhoto(response) {
   var params = {chat_id: response.message.chat.id, photo: response.message.photo[0].file_id,     reply_to_message_id:response.message.message_id, caption:response.message.photo[0].file_id};
   var url = telegramUrl + "/sendPhoto";
+  var res = await FecthPost(url,params);
+  MessageSent(res);
+}
+
+async function sendDocument(response) {
+  var params = {chat_id: response.message.chat.id, document: response.message.document.file_id,     reply_to_message_id:response.message.message_id, caption:response.message.document.file_id};
+  var url = telegramUrl + "/sendDocument";
   var res = await FecthPost(url,params);
   MessageSent(res);
 }
@@ -114,3 +119,4 @@ async function FecthPost(url,params){
   var res = await res.json();
   return res;
 }
+ 
